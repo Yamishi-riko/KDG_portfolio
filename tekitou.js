9096683494d4030a2a61b19e5dc81c7709d28fb2
山口佳恋 <yamaguchikaren@ip-10-95-3-130.ap-northeast-1.compute.internal>
2024-07-30 15:39:03 +0900

    first commit

---
+++ tekitou.js
@@ -0,0 +1,126 @@
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: true,//下部ドットナビゲーションの表示
+});
+
+particlesJS("particles-js", {
+	"particles":{
+		"number":{
+			"value":30,//この数値を変更すると桜の数が増減できる
+			"density":{
+				"enable":true,
+				"value_area":1121.6780303333778
+			}
+		},
+		"color":{
+			"value":"#fff"
+		},
+		"shape":{
+			"type":"image",//形状は画像を指定
+			"stroke":{
+				"width":0,
+			},
+			"image":{
+				"src":"img/flower.png",//【重要】画像を指定！桜の画像を設定してください。
+				"width":120,
+				"height":120
+			}
+		},
+		"opacity":{
+			"value":0.06409588744762158,
+			"random":true,
+			"anim":{
+				"enable":false,
+				"speed":1,
+				"opacity_min":0.1,
+				"sync":false
+			}
+		},
+		"size":{
+			"value":8.011985930952697,
+			"random":true,//サイズをランダムに
+			"anim":{
+				"enable":false,
+				"speed":4,
+				"size_min":0.1,
+				"sync":false
+			}
+		},
+		"line_linked":{
+			"enable":false,
+		},
+		"move":{
+			"enable":true,
+			"speed":7,//この数値を小さくするとゆっくりな動きになる
+			"direction":"bottom-right",//右下に向かって落ちる
+			"random":false,//動きはランダムにしない
+			"straight":false,//動きをとどめない
+			"out_mode":"out",//画面の外に出るように描写
+			"bounce":false,//跳ね返りなし
+			"attract":{
+				"enable":false,
+				"rotateX":281.9177489524316,
+				"rotateY":127.670995809726
+			}
+		}
+	},
+	"interactivity":{
+		"detect_on":"canvas",
+		"events":{
+			"onhover":{
+				"enable":false,
+			},
+			"onclick":{
+				"enable":false,
+			},
+			"resize":true
+		}
+	},
+	"retina_detect":false
+});
+
+
+
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200){//上から200pxスクロールしたら
+        $('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
+        $('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
+    }else{
+        if(
+            $('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
+            $('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
+            $('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
+        }
+    }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+$('body,html').animate({
+    scrollTop: 0//ページトップまでスクロール
+}, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+return false;//リンク自体の無効化
+});
